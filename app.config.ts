57a965d12cec46e3211eb77d90f87dbcf0469249
khawajad02-dev <2.75519861e+08+khawajad02-dev@users.noreply.github.com>
2026-08-25 14:37:03 +0000

    release: owner app v1.3.0

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.2.1",
+  version: "1.3.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -55,7 +55,7 @@ const config: ExpoConfig = {
       }
   },
   android: {
-    versionCode: 2,
+    versionCode: 3,
     adaptiveIcon: {
       backgroundColor: "#000000",
       foregroundImage: "./assets/images/android-icon-foreground.png",
