4501806f608f2f7dab496a4cea4da797876e990a
khawajad02-dev <2.75519861e+08+khawajad02-dev@users.noreply.github.com>
2026-08-30 20:14:37 +0000

    Refine drop and color variant workflow

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.6.6",
+  version: "1.6.7",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -55,7 +55,7 @@ const config: ExpoConfig = {
       }
   },
   android: {
-    versionCode: 12,
+    versionCode: 13,
     adaptiveIcon: {
       backgroundColor: "#000000",
       foregroundImage: "./assets/images/android-icon-foreground.png",
